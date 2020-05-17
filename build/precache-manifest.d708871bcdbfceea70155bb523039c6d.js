5a3e581be8183ee6bab8ec36e46f3154fd16ffef
Christian Krebs <christian.krebs.pi@gmail.com>
2020-05-17 12:19:31 +0200

    Added build

---
+++ build/precache-manifest.d708871bcdbfceea70155bb523039c6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b61b0c5ad342c577fa5698ad754ed1d",
+    "url": "/color-tool/build/index.html"
+  },
+  {
+    "revision": "1a6f2b6878c83b9109ca",
+    "url": "/color-tool/build/static/js/2.9706d197.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/color-tool/build/static/js/2.9706d197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f2769b8cd9a63102a3",
+    "url": "/color-tool/build/static/js/main.d220329c.chunk.js"
+  },
+  {
+    "revision": "48a97f4705bb0e7866cc",
+    "url": "/color-tool/build/static/js/runtime-main.81be106e.js"
+  }
+]);
